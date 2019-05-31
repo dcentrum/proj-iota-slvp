6c429f62500726dd254844e57ef482c0b1294512
Sreenivas Chinni <schinni.vija@gmail.com>
2019-05-31 14:38:58 +0530

    added material and ipfs packges

--- slvpwebclient/src/app/app.component.ts
+++ slvpwebclient/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MamService } from './core/mam.service';
 import { IotaService } from './core/iota.service';
-
+declare let window: any;
 
 @Component({
   selector: 'app-root',
@@ -9,10 +9,18 @@ import { IotaService } from './core/iota.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  title = 'iotangular';
+  ipfs:any;
+  title = 'tangular';
   constructor(private mam: MamService, private iota: IotaService) {
-    this.iota.getNodeInfo();
+    //this.iota.getNodeInfo();
+    window.addEventListener('load', function () {      
+      // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+      if (typeof window.IpfsHttpClient !== 'undefined') {
+        // Use Mist/MetaMask's provider
+        this.ipfs = window.IpfsHttpClient('ipfs.inpura.io', '5001')
+      }
+      console.log(this.ipfs);
+    });
   }
+  
 }
-``
