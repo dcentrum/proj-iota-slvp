272838b1a0916470df3b30ec02598110cdc567a4
Sreenivas Chinni <schinni.vija@gmail.com>
2019-07-09 18:23:08 +0530

    added http client code snippet

--- SmartCamera/index.py
+++ SmartCamera/index.py
@@ -13,14 +13,27 @@
 
 import http.client, urllib.parse
 
-#query_args = ''{ 'RPI':'Active' }''
-
-url = 'https://ipfs.infura.io/ipfs/QmTgC2pWbbAfZ5UpRYsLgi62qbcormwnA1QBH2jarFwJ8Z'
-params = urllib.parse.urlencode({'@number': 12524, '@type': 'issue', '@action': 'show'})
-conn1 = http.client.HTTPConnection(url)
-
-conn = http.client.HTTPSConnection(conn1)
-conn.request("GET", "/")
+#GET
+conn = http.client.HTTPConnection('localhost',4000)
+conn.request("GET", "/api/challans/8423")
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
+print(r1.read())
+conn.close()
+#POST
 
+
+# params = urllib.parse.urlencode({'platenum': 1234, 'geoLat': 17, 'geoLng': 57,'desc':"wrong parking"})
+# #headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+# conn1 = http.client.HTTPConnection("musi-cal.mojam.com:80")
+# conn1.request("POST", "api/ipfs/file", params)#, headers
+# response = conn1.getresponse()
+# print(response.status, response.reason)
+
+# data1 = response.read()
+# conn1.close()
+
+
+# url = 'http://jigsaw.w3.org/css-validator/validator'
+# files = {'file': open('style.css')}
+# response = requests.post(url, files=files)
