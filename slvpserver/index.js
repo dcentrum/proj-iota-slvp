7cd5347bf5001e3563a14ba7bad914e6d44b9a4a
pa1 raju <mail2chamarthi@gmail.com>
2019-07-13 20:00:59 +0530

    cors issue added

--- slvpserver/index.js
+++ slvpserver/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors')
 var appSrv = require('./AppService.js');
 var express = require('express');
 var multer = require('multer');
@@ -15,6 +16,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 models.connectDb().then(async () => {
   console.log("mongodb connected")
